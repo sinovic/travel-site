d3cc59658ce0838cce8bee1c5ccbdf1bba7fa52e
sinovic <meftah.chempart@gmail.com>
2017-08-11 22:06:44 +0100

    Completed themodal  overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import MobileMenu     from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader   from './modules/StickyHeader';
+import Modal          from './modules/Modal';
 
 var mobileMenu   = new MobileMenu();
 var stickyHeader = new StickyHeader();
+var modal        = new Modal();
 new RevealOnScroll('.feature-item', '85%');
 new RevealOnScroll('.testimonial', '60%');
+
